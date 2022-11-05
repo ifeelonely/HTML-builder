530af338321f776785fe0b4ad1b1ef50348c20b1
SmellyFinger <kooolyan123@mail.ru>
2022-11-05 14:33:04 +0300

    feat: implement fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyDir = function() {
+  const folderPath = path.join(__dirname, 'files');
+
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true}, (err) => {
+    if(err) throw new Error('Something went wrong!');
+    
+    fs.readdir(folderPath, (err, files) => {
+      for(const file of files) {
+        const filePath = path.join(folderPath, file);
+        fs.copyFile(filePath, path.join(__dirname, `files-copy`, file), (err) => {
+            if(err) throw new Error('Can not copy the file');
+        });
+      };
+
+    });
+  });
+};
+
+copyDir();
