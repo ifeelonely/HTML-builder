0d434bd95a74dbbb6596f0902f3caabb3a5889fd
SmellyFinger <kooolyan123@mail.ru>
2022-11-05 13:54:28 +0300

    feat: implement third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, `\\secret-folder`);
+fs.readdir(folderPath, (err, files) => {
+    for(const file of files) {
+        let filePath = path.join(__dirname, `\\secret-folder`, file);
+        fs.stat(filePath, {}, (err, stats) => {
+          if(stats.isFile()) 
+            console.log(`${file.split('.').join(' - ')} - ${(stats.size / 1024).toFixed(1)}кб`);
+        });
+    }; 
+});
+
+
+
